a68580dec5ebdc8b922b5f0f774879a6f354dc56
Bhupat Bheda <bhupat@noborders.net>
2021-03-03 12:03:36 +0530

    Pagination at dashboard page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from car.models import Car
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
 
 # Create your views here.
 
@@ -11,6 +14,14 @@ class DashboardListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['object_list'] = self.model.objects.all().order_by("-created")
+        objects = self.model.objects.all().order_by("-created")
+        paginator = Paginator(objects, self.paginate_by)
+        page = self.request.GET.get('page')
+        try:
+            car_list = paginator.page(page)
+        except PageNotAnInteger:
+            car_list = paginator.page(1)
+        except EmptyPage:
+            car_list = paginator.page(paginator.num_pages)
+        context['object_list'] = car_list 
         return context
-
