97cbd5d95b5b12fa3898b34d0375aa52aff7c1cb
Bhupat Bheda <bhupat@noborders.net>
2021-03-03 21:00:55 +0530

    Removed extra api and put pagination after loop

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('', DashboardListView.as_view(), name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('thank-you/', TemplateView.as_view(template_name='thank_you.html'), name="thank-you")
 ]
